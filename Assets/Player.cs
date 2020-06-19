c9d8007a830018ab38bb836ead5e623ed8f3b761
RandyRen <aznsren10@yahoo.com>
2020-06-19 18:51:34 -0400

    Horizontal movement working

---
+++ Assets/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float rawNewXPos = transform.localPosition.x + xOffset;
+
+        float clampedPos = Mathf.Clamp(rawNewXPos, -5f, 5f);
+
+        transform.localPosition = new Vector3(clampedPos, transform.localPosition.y, transform.localPosition.z);  
+    }
+}
